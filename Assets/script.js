80a4a237cd5f3cf34b3f1fcbe42a2a730ae21e91
Axel Xero <schoen9576@gmail.com>
2021-07-07 11:59:02 -0500

    added prompt into java for acuiring password length

--- Assets/script.js
+++ Assets/script.js
@@ -14,13 +14,17 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  
-    askLength;
-    askNumbers;
-    askSpecial;
-    askUpper;
-    askLower;
-    if(askLength()||askNumbers()||askSpecial()||askUpper()||askLower()){
-      
-    } 
+  // acquiring length of password and limiting it to between 8 and 128
+  var passwordLength = parseInt(prompt("How long do you want your password to be? Must be > 8 but <128"))
+  if (isNaN(passwordLength)){
+    alert("Please enter a number");
+    generatePassword()
   }
+  else if (passwordLength < 8 || passwordLength > 128){
+    alert("password must be minimum 8 characters and a maximum of 128")
+    generatePassword()
+  }
+
+      
+} 
+  
